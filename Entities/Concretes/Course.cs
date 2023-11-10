c02800299e3ca479200c64ccffa3fa04780f7714
Ahmet Özkan <ahmet.ozkan98@hotmail.com>
2023-11-10 20:17:20 +0300

    Add project files.

---
+++ Entities/Concretes/Course.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concretes
+{
+    public class Course
+    {
+        public int Id;
+        public string Title;
+        public string Description;
+        public string Teacher;
+        public string Price;
+        public string ImagePath;
+
+        public Course(int id, string title, string description, string teacher, string price, string imagePath)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            Teacher = teacher;
+            Price = price;
+            ImagePath = imagePath;
+        }
+    }
+}
